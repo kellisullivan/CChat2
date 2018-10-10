b15c8bda74a0ebd95a6dced68dfba36649af84f3
kellisullivan <k_sullivan@coloradocollege.edu>
2018-10-10 11:44:45 -0600

    Got Darryl and Sarahs stuff

---
+++ src/ChatServerProcesses.java
@@ -0,0 +1,24 @@
+import java.net.Socket;
+import java.io.IOException;
+public class ChatServerProcesses extends Thread{
+
+	private Socket socket;
+	private ChatServer chatServer;
+
+	public ChatServerProcesses(ChatServer currChatServer,Socket mySocket){
+		chatServer = currChatServer;
+		socket = mySocket;
+	}
+	public void run(){
+		try {
+			while(true){
+				String message = chatServer.read(socket);
+				chatServer.write(socket, message);
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("Something went wrong while threading...");
+			e.printStackTrace();
+		}
+	}
+}
--- src/GroupRouter.java
+++ src/GroupRouter.java
@@ -1,33 +1,93 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
 
 public class GroupRouter extends Server {
-	
+
 	private byte[] rbuf;
 	int bytesRead;
-	String request;
+	String message;
+	String prefix=new String();
+	String PING="PING";
+	HashMap<String,Integer[]> chatServers= new HashMap<String, Integer[]>();
+	int numberClients;
+	String ACPT="ACPT";
+	String LEFT= "LEFT";
+	String FWRD="FWRD";
+	String IDNT="IDNT";
+	String DENY= "DENY";
 	
 	public GroupRouter() {
-		rbuf = new byte[300];
 	}
 
 	@Override
 	public String read(Socket readSock) throws UnsupportedEncodingException, IOException {
 		// Wait for client's request and then write the request to server socket (send to server)
-		while ((bytesRead = readSock.getInputStream().read(rbuf)) > 0) {                                                                                                                               
-			request = new String(rbuf, "US-ASCII");
-		            
+		String csAddress;
+		int csPort;
+		Integer[] keyArray=new Integer[2];
+
+		InputStream stream = readSock.getInputStream();
+		Scanner scan = new Scanner(stream, "US-ASCII");
+		while (scan.hasNextLine()) {
+			message = scan.nextLine();
+			prefix=message.substring(0,3);
+			if(prefix.equals(PING)) {
+				csAddress=readSock.getInetAddress().getHostAddress().toString();
+				csPort=readSock.getPort();
+				numberClients=0;
+				keyArray[0]=csPort;
+				keyArray[1]=numberClients;
+				chatServers.put(csAddress, keyArray);
+				return null;
+			}
+			if(prefix.equals(IDNT)) {
+				Set<String> chatIPs;	
+				chatIPs=chatServers.keySet();
+				int min=0;
+				String[] chatServer=new String[2];
+				while(min<10) {
+					for(String key: chatIPs) {
+						numberClients=chatServers.get(key)[1];
+						if (numberClients==min) {
+							chatServer[0]=key;
+							chatServer[1]=chatServers.get(key)[0].toString();
+							numberClients++;
+						}
+						if(min==10) {
+							String messageTo=new String(DENY + " " + '\n');
+							return messageTo;
+						}
+					}
+					min++;
+				}
+				String messageTo= new String(ACPT + " " + chatServer[0] + " " + chatServer[1] + " " + '\n');
+				return messageTo;
+			}
+			if(prefix.equals(LEFT)) {
+				numberClients--;
+				return null;
+			}
+			if(prefix.equals(FWRD)) {
+				return message;
+			}
+			}
+		return null;
+	}
+		@Override
+		public void write(Socket writeSock, String message) throws IOException {
+			if(!message.equals(null)) {
+				writeSock.getOutputStream().write(message.getBytes("US-ASCII"),0,message.length());
+			}		
 		}
-	}
 
-	@Override
-	public void write(Socket writeSock, String message) throws IOException {
-		writeSock.getOutputStream().write(rbuf, 0, bytesRead);
-	}
+		public static void main() {
+			GroupRouter gr=new GroupRouter();
+			gr.listenConnect(ipAddress, port);
+		}
 
-	public static void main() {
-		
 	}
-
-}
